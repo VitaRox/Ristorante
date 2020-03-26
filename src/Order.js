f6080b5ead12f89e68faea8c8ef03a2a27f8cb4e
Vita <wiebetc@gmail.com>
2020-03-25 19:50:16 -0700

    - Fix some shit, especially pertaining to styling;

--- src/Order.js
+++ src/Order.js
@@ -1,45 +1,48 @@
 import React, {useState} from "react";
 // import {render} from "react-dom";
+import './App.css';
 
-// This Component gets the order, styles it, and is rendered to the ReactDOM by App;
+// This Component gets the order via a series of forms,
+// and is rendered to the ReactDOM by App.js;
 function Order() {
 
   // Hooks, which set the default state of the first parameter named after 'const';
   const [dishName, setDish] = useState("");
   const [portion, setPortion] = useState("");
-  const [specialRequest, setSpecialRequest] = useState("n/a");
+  const [specialRequest, setSpecialRequest] = useState("");
 
-    return (
-      <div>
-        <form className="orderForm">
-          <input
-            value={dishName}
-            onChange={e => setDish(e.target.value)}
-            placeholder="Name of a dish"
-            type="text"
-            name="dishName"
-            required
-          />
-          <input
-            value={portion}
-            onChange={e => setPortion(e.target.value)}
-            placeholder="Enter portion size, lunch or dinner:"
-            type="text"
-            name="portion"
-            required
-           />
-          <input
-            value={specialRequest}
-            onChange={e => setSpecialRequest(e.target.value)}
-            placeholder="Special requests?"
-            type="text"
-            name="specialRequest"
-            required
-          />
-        </form>
-      <button type="submit" className="submitButton">Submit guest order</button>
-      </div>
+  // Defines the state and general JSX design of the order-input UI;
+  return (
+    <div>
+      <form className="orderForm">
+        <input
+          value={dishName}
+          onChange={e => setDish(e.target.value)}
+          placeholder="Name of a dish"
+          type="text"
+          name="dishName"
+          required
+        />
+        <input
+          value={portion}
+          onChange={e => setPortion(e.target.value)}
+          placeholder="Enter portion size, lunch or dinner:"
+          type="text"
+          name="portion"
+          required
+         />
+        <input
+          value={specialRequest}
+          onChange={e => setSpecialRequest(e.target.value)}
+          placeholder="Special requests?"
+          type="text"
+          name="specialRequest"
+          required
+        />
+      </form>
+    </div>
     );
 };
 
+// Export this Component's functionality to App.js;
 export default Order;
