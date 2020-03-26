d391dd692a7aaeaf8de29e9ae1e5a762eca3c2d4
Vita <wiebetc@gmail.com>
2020-03-25 17:19:13 -0700

    - Add detail to App.css; attempt to style this thing; - Remember to close tags, Vita; - Finish filling out orderForm part;

--- src/Order.js
+++ src/Order.js
@@ -1,27 +1,47 @@
 import React, {useState} from "react";
-import {render} from "react-dom";
+// import {render} from "react-dom";
 
 // This Component gets the order, styles it, and is rendered to the ReactDOM by App;
-function Order({dishName, portion, specialRequests}) {
-  
+function Order() {
+
+  // Hooks, which set the default state of the first parameter named after 'const';
   const [dishName, setDish] = useState("");
-  
-  const order = () => {
+  const [portion, setPortion] = useState("");
+  const [specialRequest, setSpecialRequest] = useState("n/a");
+
+  // const order = () => {
     return (
       <div>
         <form className="orderForm">
           <input
-            value={firstName}
-            onChange={e => setFirstName(e.target.value)}
-            placeholder="First name"
+            value={dishName}
+            onChange={e => setDish(e.target.value)}
+            placeholder="Name of a dish"
             type="text"
-            name="firstName"
+            name="dishName"
+            required
+          />
+          <input
+            value={portion}
+            onChange={e => setDish(e.target.value)}
+            placeholder="Name of a dish"
+            type="text"
+            name="dishName"
+            required
+           />
+          <input
+            value={setSpecialRequest}
+            onChange={e => setDish(e.target.value)}
+            placeholder="Special requests?"
+            type="text"
+            name={specialRequest}
             required
           />
         </form>
+        <button type="submit" className="submitButton">Submit guest order</button>
       </div>
     );
-  };
+  // };
 };
 
 export default Order;
